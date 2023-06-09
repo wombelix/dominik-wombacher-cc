1832155aca2851551f5c0ac6ca88da67966d6221
Dominik Wombacher <dominik@wombacher.cc>
2023-06-09 19:35:04 +0200

    feat: SPDX license header added to rest of the config files

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+
+# SPDX-FileCopyrightText: 2023 Dominik Wombacher <dominik@wombacher.cc>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 # -*- coding: utf-8 -*- #
 
 AUTHOR = 'Dominik Wombacher'
